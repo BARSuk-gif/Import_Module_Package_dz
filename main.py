f2cb5b9bd3430bcc391699be13321c153905697b
Larisa Bastrakova <larisabastrakova0811>
2025-11-09 14:47:05 +0500

    redact commit

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 
 import datetime
 import requests
-import netologi.Import_Module_Package.application.db.people as people
-import netologi.Import_Module_Package.application.salary as salary
+from application.db.people import get_employees
+from application.salary import calculate_salary
 
 
 if __name__ == '__main__':
-    # Создаем объект сотрудника
-    employee = people.Employee(1, "Иван Иванов", "экономист")
-
-    # Расчет зарплаты (22 - рабочих дней в месяце)
-    salary_result = salary.calculate_salary(employee, 22)
-
-    print(
-        f"Для {employee} начислена зарплата: {salary_result['final_salary']:.2f} руб.")
-    print(f"Дата: {datetime.date.today()}")
+    get_employees("У Иван Иваныча")
+    calculate_salary("большая")
+    
