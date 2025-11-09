0bc1744af1c3a1c343adab9037b842d3e59f2bb7
Лариса <larisabastrakova0811@gmail.com>
2025-11-09 22:11:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ from application.salary import calculate_salary
 if __name__ == '__main__':
     get_employees("У Иван Иваныча")
     calculate_salary("большая")
-    
+    print(f"Дата: {datetime.date.today()}")
